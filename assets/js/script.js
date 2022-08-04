7b01a3204f1c7b9238d8a6376bb38dafbf9b5046
vomkaraxioned <omkarv@axioned.com>
2022-08-04 12:43:33 +0530

    dev merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,43 @@
 */
 const tabs = document.getElementsByClassName('tab');
 const info = document.getElementsByClassName('description');
-// let activeContent = document.getElementsByClassName('description active-content');
+const addBtn = document.forms['add-info']['submit'];
+// let contentIndex;
 
+addBtn.addEventListener("click", addData);
 //tabs and content change
 for (x in tabs) {
-    tabs[x].addEventListener("click", show);
+    tabs[x].addEventListener("click", tabSwitch);
 }
 
-function show() {
-    let activeTab = document.getElementsByClassName('tab active');
-    alert(activeTab);
-    activeTab[0].className = "tab";
+//tab switch function
+function tabSwitch() {
+    let activeTabs = document.getElementsByClassName('tab active');;
+    activeTabs[0].className = "tab";
     this.className += " active";
-    change();
+    showContent();
 }
 
-function change() {
+function showContent() {
+    let activeContents = document.getElementsByClassName('description active-content');
     for (x in tabs) {
         if (tabs[x].className == 'tab active') {
-            for (j in info) {
-                info[j].className = "description";
-            }
+            activeContents[0].className = "description";
             info[x].className += " active-content";
         }
     }
 }
 
-/*
+
 function addData() {
     let title = document.forms['add-info']['title'].value;
     let content = document.forms['add-info']['content'].value;
     let keyword = document.forms['add-info']['keyword'].value;
     const infoBlock = document.getElementsByClassName("info");
-    const tabs = document.getElementsByClassName("tab");
     let data = "<p class=\"description\"><span class=\"title\">${title}</span>${content}<span class=\"keyword\">${keyword}</span> powered navigation.</p>";
     let tab = "<li class=\"tab\"><a href=\"#FIXME\" title=\"${keyword}\">${keyword}</a></li>";
-    tabs.appendChild(tab);
-    infoBlock.appendChild(data);
-
+    let para = document.createElement(data);
+    let newTab = document.createElement(tab);
+    tabs.appendChild(newTab);
+    infoBlock.appendChild(para);
 }
-*/
-// addBtn.addEventListener("click", addData);
-// const addBtn = document.forms['add-info']['submit'];
